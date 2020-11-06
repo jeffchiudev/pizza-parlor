9a0b7c1f28fb20d21391e06d8dc1cfef2d17db6a
Jeff Chiu <jwchiu82@gmail.com>
2020-11-06 09:33:37 -0800

    add specs in scripts.test.js file

---
+++ _tests_/scripts.test.js
@@ -0,0 +1,22 @@
+Describe: PizzaOrder
+Test: should correctly create an object for a new pizza order 
+Code: let jeffOrder = new PizzaOrder(jeff);
+Expect: pizzaOrder.name.toEqual(jeff);
+
+Describe: NewPizzaOrder.prototype.pizzaSize();
+Test: should correctly allow user to pick pizza size 
+Expect: result.toEqual("large");
+
+Describe: NewPizzaOrder.prototype.addToppings();
+Test: should correctly allow user to pick toppings
+Expect: result.toEqual("sausage", "goat cheese");
+
+Describe: NewPizzaOrder.prototype.pizzaCost();
+Test: should correctly tabulate pizza order cost depending on size & toppings 
+Code: let jeffOrder = {"large", [sausage, goat cheese]}
+Expect: result.toEqual("$13");
+
+Describe: NewPizzaOrder.prototype.
+Test:
+Code:
+Expect:
